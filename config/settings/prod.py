31d2ba88c08ff44f5db772d9d2d8d290e78da7cf
plain-paper <94454728+plain-paper@users.noreply.github.com>
2021-11-02 14:31:06 +0300

    Remove aws s3 storage.

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -38,25 +38,6 @@ TEMPLATES[0]['OPTIONS']['loaders'] = [
 ]
 
 
-# Storage configurations
-# --------------------------------------------------------------------------
-
-AWS_STORAGE_BUCKET_NAME = env('AWS_STORAGE_BUCKET_NAME')
-AWS_ACCESS_KEY_ID = env('AWS_ACCESS_KEY_ID')
-AWS_SECRET_ACCESS_KEY = env('AWS_SECRET_ACCESS_KEY')
-AWS_AUTO_CREATE_BUCKET = True
-
-AWS_QUERYSTRING_AUTH = False
-
-AWS_S3_CUSTOM_DOMAIN = '{0}.s3.amazonaws.com'.format(AWS_STORAGE_BUCKET_NAME)
-
-STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/static/'
-MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/media/'
-
-DEFAULT_FILE_STORAGE = 'config.settings.s3utils.MediaRootS3BotoStorage'
-STATICFILES_STORAGE = 'config.settings.s3utils.StaticRootS3BotoStorage'
-
-
 # Email settings
 # --------------------------------------------------------------------------
 
