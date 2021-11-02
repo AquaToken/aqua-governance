807ec372ec7962ce1b523a91fe544c9347c6464a
wandering-cloud <108400488+wandering-cloud@users.noreply.github.com>
2021-11-02 13:24:48 +0300

    remove test task

--- aqua_governance/taskapp/__init__.py
+++ aqua_governance/taskapp/__init__.py
@@ -14,17 +14,3 @@ app = Celery('aqua_governance')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
 app.conf.timezone = 'UTC'
-
-
-@app.on_after_finalize.connect
-def setup_periodic_tasks(sender, **kwargs):
-    from drf_secure_token.tasks import DELETE_OLD_TOKENS
-
-    app.conf.beat_schedule.update({
-        'aqua_governance.testapp.tasks.test_task': {
-            'task': 'aqua_governance.testapp.tasks.test_task',
-            'run_every': timedelta(seconds=10),
-            'args': (),
-        },
-        'drf_secure_token.tasks.delete_old_tokens': DELETE_OLD_TOKENS,
-    })
