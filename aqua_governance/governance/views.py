3b4d60931cca24331f94bdc1c9e318586a74d673
wandering-cloud <108400488+wandering-cloud@users.noreply.github.com>
2021-11-11 15:34:02 +0300

    filter queryset

--- aqua_governance/governance/views.py
+++ aqua_governance/governance/views.py
@@ -17,7 +17,7 @@ from aqua_governance.governance.serializers import (
 
 
 class ProposalsView(ListModelMixin, RetrieveModelMixin, CreateModelMixin, GenericViewSet):
-    queryset = Proposal.objects.all().prefetch_related(
+    queryset = Proposal.objects.filter(hide=False).prefetch_related(
         Prefetch('logvote_set', LogVote.objects.all().order_by('-created_at')),
     )
     permission_classes = (AllowAny, )
